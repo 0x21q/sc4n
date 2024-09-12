b2d3fe7f892f4f061c871f8df34b49371bafe620
0x21q <xkrato67@stud.fit.vutbr.cz>
2024-09-12 22:31:20 +0200

    Adding working syn scan

--- go.mod
+++ go.mod
@@ -1,5 +1,21 @@
 module goscan
 
-go 1.22
+go 1.23
 
-require github.com/gopacket/gopacket v1.2.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gopacket/gopacket v1.2.0 // indirect
+	github.com/jackpal/gateway v1.0.15 // indirect
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/mdlayher/arp v0.0.0-20220512170110-6706a2966875 // indirect
+	github.com/mdlayher/ethernet v0.0.0-20220221185849-529eae5b6118 // indirect
+	github.com/mdlayher/packet v1.0.0 // indirect
+	github.com/mdlayher/socket v0.2.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- scan/syn.go
+++ scan/syn.go
@@ -3,36 +3,112 @@ package scan
 import (
 	"fmt"
 	"net"
+	"net/netip"
 	"strings"
+	"time"
 
 	"github.com/gopacket/gopacket"
 	"github.com/gopacket/gopacket/layers"
 	"github.com/gopacket/gopacket/pcap"
+
+	// needed for the gateway MAC discovery
+	"github.com/jackpal/gateway"
+	"github.com/mdlayher/arp"
 )
 
 func Syn(hosts []net.IP, ports []uint16, iface net.Interface) {
 	selected := SelectHost(hosts, true)
 
-	handle, err := pcap.OpenLive(iface.Name, 65535, false, pcap.BlockForever)
+	handleSend, err := pcap.OpenLive(iface.Name, 65535, false, time.Millisecond*10)
 	if err != nil {
 		fmt.Printf("failed to open device: %v\n", err)
 	}
-	defer handle.Close()
+	defer handleSend.Close()
+
+	synackFilter := "(tcp[tcpflags] & (tcp-syn|tcp-ack) == (tcp-syn|tcp-ack))"
+	rstackFilter := "(tcp[tcpflags] & (tcp-rst|tcp-ack) == (tcp-rst|tcp-ack))"
+	bpfFilter := synackFilter + " or " + rstackFilter + " and src host " + selected.String()
 
 	for _, port := range ports {
-		packet, err := createSynPacket(selected, port, iface)
-		if err != nil {
+		if err := sendSynPacket(selected, port, iface, handleSend); err != nil {
 			fmt.Printf("failed to create SYN packet: %v\n", err)
 			continue
 		}
-		handle.WritePacketData(packet)
-		fmt.Println("sent SYN packet")
+
+		handleListen, err := pcapListen(iface.Name, bpfFilter)
+		if err != nil {
+			fmt.Printf("failed to open device: %v\n", err)
+			continue
+		}
+		defer handleListen.Close()
+
+		packetType := receivePacketTCP(handleListen, time.Second)
+		if packetType == "SYN-ACK" {
+			fmt.Printf("%d/tcp %6s\n", port, "open")
+		} else {
+			fmt.Printf("%d/tcp %6s\n", port, "closed")
+		}
 	}
 }
 
-func createSynPacket(host net.IP, port uint16, iface net.Interface) ([]byte, error) {
+func sendSynPacket(host net.IP, port uint16, iface net.Interface, sendHandle *pcap.Handle) error {
 	buf := gopacket.NewSerializeBuffer()
 
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	ethH := layers.Ethernet{
+		DstMAC:       getNextHopMAC(iface),
+		SrcMAC:       iface.HardwareAddr,
+		EthernetType: layers.EthernetTypeIPv4,
+	}
+
+	srcIP, err := getInterfaceIP(iface)
+	if err != nil {
+		return fmt.Errorf("failed to get interface IP: %v", err)
+	}
+
+	ipH := layers.IPv4{
+		DstIP:    host,
+		SrcIP:    srcIP,
+		Protocol: layers.IPProtocolTCP,
+		Version:  4,
+		TTL:      64,
+		IHL:      5,
+		Id:       33333,
+	}
+
+	tcpH := layers.TCP{
+		DstPort: layers.TCPPort(port),
+		SrcPort: layers.TCPPort(59595),
+		SYN:     true,
+		Seq:     123456789,
+		Window:  1024,
+		Options: []layers.TCPOption{
+			{
+				OptionType:   layers.TCPOptionKindMSS,
+				OptionLength: 4,
+				OptionData:   []byte{0x05, 0xb4},
+			},
+		},
+	}
+
+	tcpH.SetNetworkLayerForChecksum(&ipH)
+
+	if err := gopacket.SerializeLayers(buf, opts, &ethH, &ipH, &tcpH); err != nil {
+		return fmt.Errorf("failed to serialize layers: %v", err)
+	}
+
+	if err := sendHandle.WritePacketData(buf.Bytes()); err != nil {
+		return fmt.Errorf("failed to write packet data: %v", err)
+	}
+
+	return nil
+}
+
+func getInterfaceIP(iface net.Interface) (net.IP, error) {
 	addrs, err := iface.Addrs()
 	if err != nil {
 		return nil, fmt.Errorf("failed to get interface addresses: %v", err)
@@ -42,31 +118,94 @@ func createSynPacket(host net.IP, port uint16, iface net.Interface) ([]byte, err
 		return nil, fmt.Errorf("interface has no addresses")
 	}
 
-	opts := gopacket.SerializeOptions{
-		FixLengths:       true,
-		ComputeChecksums: true,
+	if len(strings.Split(addrs[0].String(), "/")) != 2 {
+		return nil, fmt.Errorf("invalid address format")
 	}
 
-	srcIPStr := strings.Split(addrs[0].String(), "/")[0]
+	return net.ParseIP(strings.Split(addrs[0].String(), "/")[0]), nil
+}
 
-	ipH := layers.IPv4{
-		DstIP:    host,
-		SrcIP:    net.ParseIP(srcIPStr),
-		Protocol: layers.IPProtocolTCP,
+func receivePacketTCP(handle *pcap.Handle, timeout time.Duration) string {
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	res := make(chan bool)
+	packetType := ""
+
+	go waitForPacketTCP(packetSource, res, timeout, &packetType)
+
+	<-res
+	return packetType
+
+}
+
+func waitForPacketTCP(
+	source *gopacket.PacketSource,
+	res chan<- bool,
+	timeout time.Duration,
+	packetType *string,
+) {
+	for {
+		select {
+		case packet := <-(*source).Packets():
+			if packet == nil {
+				continue
+			}
+
+			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+			if tcpLayer == nil {
+				continue
+			}
+			tcp, _ := tcpLayer.(*layers.TCP)
+
+			if tcp.SYN && tcp.ACK {
+				*packetType = "SYN-ACK"
+				res <- true
+				return
+			}
+
+			if tcp.RST && tcp.ACK {
+				*packetType = "RST-ACK"
+				res <- true
+				return
+			}
+		case <-time.After(timeout):
+			*packetType = ""
+			res <- true
+			return
+		}
+	}
+}
+
+func getNextHopMAC(iface net.Interface) net.HardwareAddr {
+	gw, err := gateway.DiscoverGateway()
+	if err != nil {
+		fmt.Printf("failed to get gateway: %v\n", err)
 	}
 
-	tcpH := layers.TCP{
-		DstPort: layers.TCPPort(port),
-		SrcPort: layers.TCPPort(12345),
-		SYN:     true,
+	mac, err := sendARP(gw, iface)
+	if err != nil {
+		fmt.Printf("failed to send ARP request to next hop device: %v\n", err)
+		return net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}
 	}
 
-	tcpH.SetNetworkLayerForChecksum(&ipH)
+	return mac
+}
 
-	errS := gopacket.SerializeLayers(buf, opts, &ipH, &tcpH)
-	if errS != nil {
-		return nil, errS
+func sendARP(gateway net.IP, iface net.Interface) (net.HardwareAddr, error) {
+	client, err := arp.Dial(&iface)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial ARP: %v\n", err)
+	}
+	defer client.Close()
+
+	ip, ok := netip.AddrFromSlice(gateway.To4())
+	if !ok {
+		return nil, fmt.Errorf("failed to convert IP: %v\n", err)
 	}
 
-	return buf.Bytes(), nil
+	mac, err := client.Resolve(ip)
+	if err != nil {
+		return nil, fmt.Errorf("failed to resolve MAC: %v\n", err)
+	}
+
+	return mac, nil
 }
